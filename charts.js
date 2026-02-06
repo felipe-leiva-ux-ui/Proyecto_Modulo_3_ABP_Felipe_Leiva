349deec6fd17e9c5351908ea4c2b9950259a15d1
felipe-leiva-ux-ui <felipeandresleivaneira@gmail.com>
2026-02-06 02:54:19 -0300

    dashboard v1.0

---
+++ charts.js
@@ -0,0 +1,52 @@
+const getOptionsChart1 = () => {
+  return {
+    tooltip: {
+      trigger: 'item',
+    },
+    legend: {
+      top: '5%',
+      left: 'center',
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: ['40%', '70%'],
+        avoidLabelOverlap: false,
+        itemStyle: {
+          borderRadius: 10,
+          borderColor: '#fff',
+          borderWidth: 2,
+        },
+        label: {
+          show: false,
+          position: 'center',
+        },
+        emphasis: {
+          label: {
+            show: true,
+            fontSize: 40,
+            fontWeight: 'bold',
+          },
+        },
+        labelLine: {
+          show: false,
+        },
+        data: [
+          { value: 1048, name: 'Compras' },
+          { value: 735, name: 'Cuentas básicas ' },
+          { value: 580, name: 'Suscripciones' },
+        ],
+      },
+    ],
+  };
+};
+
+const initCharts = () => {
+  const chart1 = echarts.init(document.getElementById('chart1'));
+  chart1.setOption(getOptionsChart1());
+};
+
+window.addEventListener('load', () => {
+  initCharts();
+});
